27ddd92236c4cf56740ae14c932f99f7e4471653
TaQini <742954809@qq.com>
2018-03-07 10:42:37 +0800

    xxx

--- tools/config.sh
+++ tools/config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd ~
 sudo apt-get update
-sudo apt-get install vim git zsh
+sudo apt-get install -y vim git zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 # echo 'ZSH_THEME="jtriley"' >> ./.zshrc
 
